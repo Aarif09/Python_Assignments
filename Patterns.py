c0df94061987ac8722772e43a3c44740201a179a
Arif <ashaikhsacred@gmail.com>
2025-01-21 10:08:34 +0530

    Adding Python Codes

---
+++ Patterns.py
@@ -0,0 +1,20 @@
+
+import re
+Pattern_un=r"[A-z,0-9]*\"\n",
+Pattern_psw=r"[A-z,0-9@#$%^&+]{6,20}$\"\n"
+Pattern_name=r"^[A-Z][a-z]*$\"\n",
+Pattern_email=r"[A-z0-9]+.+@+[A-z]+.+[a-z]$\"\n",
+Pattern_phone=r"^[0-9]{10}$\"\n",
+Pattern_lic=r"[A-Z]{2}[0-9]{2}[0-9]{11}$\"\n",
+Pattern_adhr=r"[0-9]{12}$\"\n",
+Pattern_city=r"^[A-Z,a-z]+$\"\n",
+Pattern_pincode=r"^[0-9]{6}$\"\n",
+Pattern_state=r"^[A-Z,a-z]+$\"\n",
+Pattern_Company_name=r"^[A-Z,a-z]+$\"\n",
+Pattern_mdl=r"[A-z][0-9]*\"\n",
+Pattern_Color=r"^[A-Z,a-z]+$\"\n",
+Pattern_Price=r"^[0-9,A-z]+$\"\n",
+Patter_Fuel=['Petrol','Diesel','Ev','Cng']
+   
+  
+
